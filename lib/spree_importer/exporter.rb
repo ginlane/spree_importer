8578a974d6566c3eedbb5899a7cd4848cc47e3d0
Scotty Weeks <scott.weeks@gmail.com>
2013-10-30 11:12:20 -0400

    puts statements?

--- lib/spree_importer/exporter.rb
+++ lib/spree_importer/exporter.rb
@@ -37,7 +37,7 @@ class SpreeImporter::Exporter
   end
 
   def get_exporters(exporters)
-    puts "FNORD "
+    puts "FNODR "
     if exporters.nil?
       SpreeImporter.config.exporters.values
     else
