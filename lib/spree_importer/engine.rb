07cfe71d20a8b3cd8c6d2a49715ae150de48c1bf
Sebastian Bean <sebastian@ginlanemedia.com>
2013-10-31 16:50:10 -0400

    keep migrations inside the engine

--- lib/spree_importer/engine.rb
+++ lib/spree_importer/engine.rb
@@ -6,6 +6,14 @@ module SpreeImporter
 
     config.autoload_paths += %W(#{config.root}/lib)
 
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+    
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
