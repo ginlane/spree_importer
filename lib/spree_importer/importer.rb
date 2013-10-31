b0d70c59356af13a9e63095ab427b3ac2f004035
Scotty Weeks <scott.weeks@gmail.com>
2013-10-31 17:10:30 -0400

    force utf-8 encoding on imported csvs

--- lib/spree_importer/importer.rb
+++ lib/spree_importer/importer.rb
@@ -30,7 +30,7 @@ module SpreeImporter
     end
 
     def parse
-      self.csv     = CSV.parse csv, headers: :first_row
+      self.csv     = CSV.parse csv.force_encoding("UTF-8"), headers: :first_row, encoding: "UTF-8"
       self.headers = Hash[csv.headers.map.with_index.to_a].inject({ }) do |hs, (k, _)|
         h               = Field.new k, is_header = true
         hs[h.sanitized] = h
