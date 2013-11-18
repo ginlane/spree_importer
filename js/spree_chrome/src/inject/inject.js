cc580b903e8ed3ce322bb2d3c4ef3e13e1c3f606
Scotty Weeks <scott.weeks@gmail.com>
2013-11-18 15:43:37 -0500

    removed log messages.

--- js/spree_chrome/src/inject/inject.js
+++ js/spree_chrome/src/inject/inject.js
@@ -12,14 +12,13 @@ chrome.extension.sendMessage({}, function(response) {
         pair = url[i].split("=");
         params[pair[0]] = pair[1];
       }
+
       http.onreadystatechange = function() {
-        console.log(http.status);
-        console.log(http.readyState);
-        console.log(http.responseText);
         if(http.readyState === 4 && /Authorization needed/.test(http.responseText)) {
           window.open(APP_URL, "_blank");
         }
       };
+
       http.open("GET", APP_URL + "?spreadsheetId=" + params.key, true);
       http.send();
 	  }
