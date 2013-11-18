d9e96e9912f202f3790f93cc2c8f8b7cad746428
Sebastian Bean <sebastian@ginlanemedia.com>
2013-11-18 13:17:17 -0500

    disabled auto merge migrations path

--- lib/spree_importer/engine.rb
+++ lib/spree_importer/engine.rb
@@ -6,13 +6,13 @@ module SpreeImporter
 
     config.autoload_paths += %W(#{config.root}/lib)
 
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
-        end
-      end
-    end
+    # initializer :append_migrations do |app|
+    #   unless app.root.to_s.match root.to_s
+    #     config.paths["db/migrate"].expanded.each do |expanded_path|
+    #       app.config.paths["db/migrate"] << expanded_path
+    #     end
+    #   end
+    # end
     
     # use rspec for tests
     config.generators do |g|
