6bea6eee6409283cab5e9e5414149d13b32ccf56
Scotty Weeks <scott.weeks@gmail.com>
2013-12-17 14:51:23 -0500

    routes prpend

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,18 @@
-Spree::Core::Engine.routes.draw do
-  routes = lambda do
-    get "/admin/products/export"  => "admin/products#export", as: :admin_export_products
-    get "/admin/orders/export"    => "admin/order_exports#export", as: :admin_export_orders
+Spree::Core::Engine.routes.prepend do
+  get "/admin/products/export"  => "admin/products#export", as: :admin_export_products
+  get "/admin/orders/export"    => "admin/order_exports#export", as: :admin_export_orders
 
-    namespace :admin do
-      resources :imports
-      resources :import_source_files do
-        put  :edit_in_google
-        post :create_from_url, on: :collection
-        post :import_from_google
-        get  :show_in_google
-      end
-      get "/auth/check_google"    => "oauth#check_google", as: :check_google
-      get "/auth/google"          => "oauth#google", as: :google_auth
-      get "/auth/google_redirect" => "oauth#google_redirect"
+  namespace :admin do
+    resources :imports
+    resources :import_source_files do
+      put  :edit_in_google
+      post :create_from_url, on: :collection
+      post :import_from_google
+      get  :show_in_google
     end
+    get "/auth/check_google"    => "oauth#check_google", as: :check_google
+    get "/auth/google"          => "oauth#google", as: :google_auth
+    get "/auth/google_redirect" => "oauth#google_redirect"
   end
 
-  if Spree::Core::Engine.respond_to? :add_routes
-    Spree::Core::Engine.add_routes &routes
-  else
-    Spree::Core::Engine.routes.draw &routes
-  end
 end
