459a70a6865d2b8f7d485258a1d341cd20e49dc4
Denis Ivanov <visible@jumph4x.net>
2014-01-08 13:14:09 -0800

    Adding inverse :belongs_to relations

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,8 @@
 module Spree
   Product.class_eval do
+
+    belongs_to :import_source_file, foreign_key: :batch_id
+
     def set_sku(variant)
       pattern         = sku_pattern.blank?? SpreeImporter.config.default_sku : sku_pattern
       options         = variant.option_values.map {|v| [ v.option_type.name, v ] }
