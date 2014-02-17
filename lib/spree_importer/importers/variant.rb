4a0af37ddead5629d30189baa19d7fdecd94e622
Sebastian Bean <sebastian@ginlanemedia.com>
2014-02-17 14:56:19 -0500

    fixed bug with flat imports generating extra variant

--- lib/spree_importer/importers/variant.rb
+++ lib/spree_importer/importers/variant.rb
@@ -14,7 +14,8 @@ module SpreeImporter
 
       def import(headers, csv)
         each_instance headers, csv do |instance, row|
-          next if val(headers, row, :sku) == val(headers, row, :master_sku)
+          next if val(headers, row, :sku) == val(headers, row, :master_sku) ||
+                  val(headers, row, :sku).blank?
 
           if instance.new_record?
             product          = master_variant(headers, row).product
--- spec/lib/spree_importer/importers/property_spec.rb
+++ spec/lib/spree_importer/importers/property_spec.rb
@@ -8,4 +8,10 @@ describe SpreeImporter::Importers::Property do
     property.presentation.should eql "Colors"
     property.name.should == "color"
   end
+  it "should not create more variants than in flat spreadsheet" do
+    import_source_file = get_import_source_file "gin-lane-product-list"
+    import_source_file.import!
+
+    Spree::Variant.where(sku: 'STN-FW13-WAVE-SHIRT').first.product.variants.count.should eq 12
+  end
 end
