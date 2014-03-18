b8d5f4517a73725c9c5dd9068af2d25eada311c7
Sebastian Bean <sebastian@ginlanemedia.com>
2014-03-18 15:53:52 -0400

    no longer needs separator for sku pattern

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -7,7 +7,14 @@ module Spree
       pattern         = sku_pattern.blank?? SpreeImporter.config.default_sku : sku_pattern
       options         = variant.option_values.map {|v| [ v.option_type.name, v ] }
       @interpolator   = SpreeImporter::SkuInterpolator.new(pattern)
-      variant.sku     = @interpolator.to_sku master.sku, options
+      variant.update sku: @interpolator.to_sku(master.sku, options)
+      variant.sku
+    end
+
+    def reset_variants_skus
+      variants.map do |v|
+        set_sku(v)
+      end
     end
   end
 end
