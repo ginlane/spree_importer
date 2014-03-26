28903392d55b0ba6ed9e3856129a585e88294876
Sebastian Bean <sebastian@ginlanemedia.com>
2014-03-26 06:35:57 -0400

    buno

--- lib/spree_importer/importers/product.rb
+++ lib/spree_importer/importers/product.rb
@@ -23,8 +23,8 @@ module SpreeImporter
 
           # for safety we're skipping and warning on products that look like dups
           if ::Spree::Variant.exists? sku: product.sku
-            puts "PROD EXISTS"
-            Spree::Product.by_sku(product.sku).update_all(batch_id: batch_id)
+            # puts "PROD EXISTS"
+            # Spree::Product.by_sku(product.sku).update_all(batch_id: batch_id)
             # ap product
             # self.warnings << "Product exists for sku #{product.sku}, skipping product import"
             next
