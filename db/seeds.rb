2006714301cf78cd55a652eaa4757235711628de
Sebastian Bean <sebastian@ginlanemedia.com>
2014-04-09 19:20:07 -0400

    fix for Binding seed bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,23 @@ import_path = "#{Rails.root}/db/import.csv"
 if File.exist?(import_path)
   data     = File.read import_path
   SpreeImporter.config.progress_logging_enabled = true
-  importer = Spree::ImportSourceFile.new data: data, file_name: import_path, mime: "text/csv"
-  importer.save
-  importer.import!
+  importer     = SpreeImporter::Importer.new
+  importer.csv = data
+  importer.parse  
+  
+  importer.option_headers.each do |header|
+    importer.import :option, option_name: header.key, create_record: true
+  end
+  importer.property_headers.each do |header|
+    importer.import :property, property_name: header.key, create_record: true
+  end
+
+  importer.prototype_headers.each do |header|
+    importer.import :prototype, prototype_name: header.key, create_record: true
+  end
+  importer.import :taxonomy
+  
+  importer.import :product, batch_id: id
+  importer.import :variant, batch_id: id
+
 end
