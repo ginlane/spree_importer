f49e1a4a52ec03760cc6e4499328a6e53baba8b4
Sebastian Bean <sebastian@ginlanemedia.com>
2014-05-02 14:06:46 -0400

    no longer reorders taxon if exists

--- lib/spree_importer/importers/taxonomy.rb
+++ lib/spree_importer/importers/taxonomy.rb
@@ -21,10 +21,11 @@ module SpreeImporter
 
             value.split(delimiter).each do |heirarchy|
               heirarchy = heirarchy.split(sep).map &:strip
+              
               taxonomy  = ::Spree::Taxonomy.find_or_create_by name: heirarchy.shift
               heirarchy.inject(taxonomy.root) do |taxon, sub|
                 sub_taxon = ::Spree::Taxon.where(name: sub, taxonomy: taxonomy).first_or_create
-                sub_taxon.move_to_child_of taxon
+                sub_taxon.move_to_child_of(taxon) unless sub_taxon.parent == taxon
                 sub_taxon
               end
 
