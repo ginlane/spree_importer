55b4a08a1613495b23372d481d096a0c3f172e0b
Sebastian Bean <sebastian@ginlanemedia.com>
2014-05-06 21:43:17 -0400

    clearnt up

--- lib/spree_importer/engine.rb
+++ lib/spree_importer/engine.rb
@@ -6,14 +6,6 @@ module SpreeImporter
 
     config.autoload_paths += %W(#{config.root}/lib)
 
-    # initializer :append_migrations do |app|
-    #   unless app.root.to_s.match root.to_s
-    #     config.paths["db/migrate"].expanded.each do |expanded_path|
-    #       app.config.paths["db/migrate"] << expanded_path
-    #     end
-    #   end
-    # end
-    
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
