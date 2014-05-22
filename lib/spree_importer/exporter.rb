efe718d1613728d10f9c96e68e453cd096fa6561
Sebastian Bean <sebastian@ginlanemedia.com>
2014-05-22 14:19:48 -0400

    switch from reset to progress=0 via cron error

--- lib/spree_importer/exporter.rb
+++ lib/spree_importer/exporter.rb
@@ -32,20 +32,20 @@ class SpreeImporter::Exporter
       end
 
       if SpreeImporter.config.progress_logging_enabled
-        pbar = ::ProgressBar.create(title:self.class.name.demodulize.pluralize)      
-      end      
+        pbar = ::ProgressBar.create(title:self.class.name.demodulize.pluralize)
+      end
       target_exporter.each_export_item options[:search] do |export_item|
         if SpreeImporter.config.progress_logging_enabled
           # pbar.title = export_item.try(:name) || export_item.to_s
-          if pbar.total != target_exporter.collection.try(:count) 
-            pbar.reset
-            pbar.total = target_exporter.collection.try(:count)             
+          if pbar.total != target_exporter.collection.try(:count)
+            pbar.progress = 0
+            pbar.total = target_exporter.collection.try(:count)
           end
           pbar.increment
         end
 
         row = CSV::Row.new headers, [ ]
-        
+
         exporters.each do |exporter|
           exporter.append row, export_item
         end
@@ -56,7 +56,7 @@ class SpreeImporter::Exporter
           csv << row
         end
       end
-      pbar.stop if SpreeImporter.config.progress_logging_enabled      
+      pbar.stop if SpreeImporter.config.progress_logging_enabled
     end
   end
 
